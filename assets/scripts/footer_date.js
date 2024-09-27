021e52ca90f33f6bb4bfa2f4c00f9357abe80d3a
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2024-09-27 01:18:33 -0300

    chore: ending footer

---
+++ assets/scripts/footer_date.js
@@ -0,0 +1,4 @@
+const paraph = document.querySelector(".footer_info");
+const year = new Date().getFullYear();
+
+paraph.textContent = `© ${year} - Cosmos Enterprise - Todos os direitos reservados.`;
